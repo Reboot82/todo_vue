9f3182505b70564ef73f634461fbba3964cf8d0b
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2020-01-26 18:20:21 -0500

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What you gotta do?" v-model="newTodo" />
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+        {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo-list",
+  data() {
+    return {
+      newTodo: "",
+      todos: [
+        {
+          id: 1,
+          title: "Finish Vue Screen",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Beat this level",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Get a job",
+          completed: false
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+}
+</style>
