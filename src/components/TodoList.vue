c0781f7ad1bf3f86fe23eb4126befd9fc5bdd246
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2020-01-26 20:40:17 -0500

    remove function works

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    <input type="text" class="todo-input" placeholder="What you gotta do?" 
-    v-model="newTodo" @keyup.enter="addTodo" />
-    <div v-for="todo in todos" :key="todo.id" class="todo-item">
-        {{ todo.title }}
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="What you gotta do?"
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+    />
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div>{{ todo.title }}</div>
+      <div class="remove-item" @click="removeTodo(index)">
+          &times;
+      </div>
     </div>
   </div>
 </template>
@@ -35,16 +43,23 @@ export default {
     };
   },
   methods: {
-      addTodo() {
-          this.todos.push({
-              id: this.idForTodo,
-              title: this.newTodo,
-              completed: false,
-          })
-
-          this.newTodo = ''
-          this.idForTodo++
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return;
       }
+
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      });
+
+      this.newTodo = "";
+      this.idForTodo++;
+    },
+    removeTodo(index) {
+        this.todos.splice(index, 1)
+    }
   }
 };
 </script>
@@ -57,4 +72,21 @@ export default {
   font-size: 18px;
   margin-bottom: 16px;
 }
+
+.todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+}
+
+.remove-item:hover {
+        color: black;
+    }
+
 </style>
